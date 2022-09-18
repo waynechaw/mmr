43f201c86883fb898c1b4676281cb53b613cf910
Wayne Chaw <wayne.chaw@gmail.com>
2022-09-18 14:16:07 -0700

    get latest match history

--- index.js
+++ index.js
@@ -95,58 +95,84 @@ app.get('/mmr/:name', function (req, res) {
 
       encryptedID = encryptedID[1];
 
-      request(`https://na.op.gg/api/games/na/summoners/${encryptedID}?hl=en_US&game_type=NORMAL`, function (error, response, body) {
+      request.post(`https://na.op.gg/api/summoners/na/${encryptedID}/renewal`, function (error, response, body) {
 
-        let jsonData = JSON.parse(body);
-        jsonData.data = jsonData.data.filter(match => match.average_tier_info);
 
-        if (jsonData.data.length == 0) {
-          return res.send('no matches for this user')
+        const intervalID = setInterval( checkRenewal, 500);
+
+        function checkRenewal(){
+
+          request(`https://na.op.gg/api/summoners/na/${encryptedID}/renewal-status`, function (error, response, body) {
+              body = JSON.parse(body);
+
+              if (body && ((body.message == 'Already renewed.') || (body.message == 'Failed to renew.'))) {
+
+                clearInterval(intervalID);
+                console.log('clearInterval');
+
+                request(`https://na.op.gg/api/games/na/summoners/${encryptedID}?hl=en_US&game_type=NORMAL`, function (error, response, body) {
+
+                  let jsonData = JSON.parse(body);
+                  jsonData.data = jsonData.data.filter(match => match.average_tier_info);
+
+                  if (jsonData.data.length == 0) {
+                    return res.send('no matches for this user')
+                  }
+                  let recentMatchesTiers = jsonData.data.map(match => match.average_tier_info.tier + match.average_tier_info.division);
+                  let recentMatchesAvgMMR = jsonData.data.map(match => rankToMMR[match.average_tier_info.tier + match.average_tier_info.division]).reduce((a, b) => a + b)/jsonData.data.length;
+
+
+                  let mmrRows = '';
+
+                  for (const property in rankToMMR) {
+                    mmrRows = mmrRows + `
+                      <tr>
+                        <td>${property}</td>
+                        <td>${rankToMMR[property]}</td>
+                      </tr>
+                    `;
+                  }
+
+                  res.send(`
+
+                    <h1>Simple MMR Checker</h1>
+
+                    <p>This tool checks user's MMR in normals. Currently only works for NA. <br> 
+                    To get the most accurate mmr, <b>play 20 games solo in normals</b>. Queueing with other players will cause inaccurate result.<br>
+                    The tool uses below table to convert rank to mmr. This is mostly a guess, so contact wayne to help improve this table</p>      
+
+                    <div><b>Name: </b> ${req.params.name}</div>
+                    <div><b>Recent Matches Avg MMR: </b> ${recentMatchesAvgMMR}</div>
+                    <div><b>Number of Matches Analyzed: </b> ${jsonData.data.length}</div>
+                    <div><b>Recent Matches Tiers: </b> ${recentMatchesTiers}</div>
+                    <br><br>
+                    <table>
+                      <tr>
+                        <th>Rank</th>
+                        <th>MMR</th>
+                      </tr>
+                      ${mmrRows}
+                    </table>
+                  `);
+
+                  // res.json({
+                  //   user: req.params.name,
+                  //   recentMatchesAvgMMR: recentMatchesAvgMMR,
+                  //   numberOfMatchesAnalyzed: jsonData.data.length,
+                  //   recentMatchesTiers: recentMatchesTiers,
+                  //   rankToMMR: rankToMMR,
+                  // });
+
+                });
+
+
+
+              }
+
+          })
         }
-        let recentMatchesTiers = jsonData.data.map(match => match.average_tier_info.tier + match.average_tier_info.division);
-        let recentMatchesAvgMMR = jsonData.data.map(match => rankToMMR[match.average_tier_info.tier + match.average_tier_info.division]).reduce((a, b) => a + b)/jsonData.data.length;
 
 
-        let mmrRows = '';
-
-        for (const property in rankToMMR) {
-          mmrRows = mmrRows + `
-            <tr>
-              <td>${property}</td>
-              <td>${rankToMMR[property]}</td>
-            </tr>
-          `;
-        }
-
-        res.send(`
-
-          <h1>Simple MMR Checker</h1>
-
-          <p>This tool checks user's MMR in normals. Currently only works for NA. <br> 
-          To get the most accurate mmr, <b>play 20 games solo in normals</b>. Queueing with other players will cause inaccurate result.<br>
-          The tool uses below table to convert rank to mmr. This is mostly a guess, so contact wayne to help improve this table</p>      
-
-          <div><b>Name: </b> ${req.params.name}</div>
-          <div><b>Recent Matches Avg MMR: </b> ${recentMatchesAvgMMR}</div>
-          <div><b>Number of Matches Analyzed: </b> ${jsonData.data.length}</div>
-          <div><b>Recent Matches Tiers: </b> ${recentMatchesTiers}</div>
-          <br><br>
-          <table>
-            <tr>
-              <th>Rank</th>
-              <th>MMR</th>
-            </tr>
-            ${mmrRows}
-          </table>
-        `);
-
-        // res.json({
-        //   user: req.params.name,
-        //   recentMatchesAvgMMR: recentMatchesAvgMMR,
-        //   numberOfMatchesAnalyzed: jsonData.data.length,
-        //   recentMatchesTiers: recentMatchesTiers,
-        //   rankToMMR: rankToMMR,
-        // });
 
       });
 
